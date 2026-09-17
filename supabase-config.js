6d8c78a2894855f644a51d16f9bc5cf2dfdad725
fkahs <alswl100409@gmail.com>
2026-09-17 14:31:44 +0900

    Connect Supabase client

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'PASTE_YOUR_SUPABASE_PROJECT_URL_HERE';
-const SUPABASE_ANON_KEY = 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://ywkbzoprvdnbtfdyzlye.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_UY4_5H0ZBRsg82UeLR2vZA_VKmw60_Q';
 
 const SUPABASE_CONFIGURED = !SUPABASE_URL.startsWith('PASTE_') && !SUPABASE_ANON_KEY.startsWith('PASTE_');
 const diarySupabase = SUPABASE_CONFIGURED
